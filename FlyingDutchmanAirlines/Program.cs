dbe66e3e46c140f94a91f12d4a923544994143a7
echao12 <echao0077@gmail.com>
2021-12-17 23:10:00 -0800

    initial implementation of program file to create a host.

--- FlyingDutchmanAirlines/Program.cs
+++ FlyingDutchmanAirlines/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;//for IHost
 
 namespace FlyingDutchmanAirlines
 {
@@ -7,7 +9,17 @@ namespace FlyingDutchmanAirlines
     {
         static void Main(string[] args) // etry point to all C# programs
         {
-            
+            // note: Host lives inside common language runtime. Web service lives inside host.
+            //create a host to run the web service.
+            //host process is responsible for app startup & lifetime management.
+            //minimum req: Host configures server & request processing pipelines
+            IHost host = 
+                Host.CreateDefaultBuilder() //returns a hostbulder with default settings
+                        .ConfigureWebHostDefaults( builder =>
+                            {
+                                builder.UseUrls("http://0.0.0.0:8080"); //specify url&port we wanna use for this builder
+                            }).Build(); //build & return resulting host.
+                host.Run(); // start host
         }
     }
 }
