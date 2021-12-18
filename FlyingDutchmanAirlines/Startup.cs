19e126bcb0de2caabaa5b469d4129d989b1c5d48
echao12 <echao0077@gmail.com>
2021-12-18 00:20:28 -0800

    updated Startup with ConfigureServices
    - configure services fn adds controller services to the app.

--- FlyingDutchmanAirlines/Startup.cs
+++ FlyingDutchmanAirlines/Startup.cs
@@ -1,19 +1,26 @@
 using Microsoft.AspNetCore.Builder; // for IApplicationBuilder class
+using Microsoft.Extensions.DependencyInjection;//for IServicesCollection
 
 namespace FlyingDutchmanAirlines {
     class Startup {
         //configure called by HostBuilder & contains configuration info about the app.
-        void Configure(IApplicationBuilder app) {
+        public void Configure(IApplicationBuilder app) {
             //enable routing to endpoints
             app.UseRouting();
             //enables ability for us to use/specify endpoints
             //takes Action type as an argument. Action is a delegate.
-                //note: delegates is a way to reference methods. can only point to methods with a given signature.
+                //NOTE: delegates is a way to reference methods. can only point to methods with a given signature.
                 //delegates created in 3 ways: delegate keyword, anonymous method, lambda expression.
                 //we are using a lambda expression for this Action delegate.
 
             //Map.Controllers scans codebase for any controllers & generates the appropriate routes to the endpoints in the controllers.
             app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+
+        //Config Services is called by host first before Configure().
+        //add services to the application to use.
+        public void ConfigureServices(IServiceCollection services){
+            services.AddControllers();
         }
     }
 }
