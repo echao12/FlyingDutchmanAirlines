4fedb6c89acfd3583a5360d4606cf6b7ac22cee5
echao12 <echao0077@gmail.com>
2021-12-18 00:14:30 -0800

    added startup class & implemented Configure fn
    configure adds routing & endpoints to the application.

---
+++ FlyingDutchmanAirlines/Startup.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Builder; // for IApplicationBuilder class
+
+namespace FlyingDutchmanAirlines {
+    class Startup {
+        //configure called by HostBuilder & contains configuration info about the app.
+        void Configure(IApplicationBuilder app) {
+            //enable routing to endpoints
+            app.UseRouting();
+            //enables ability for us to use/specify endpoints
+            //takes Action type as an argument. Action is a delegate.
+                //note: delegates is a way to reference methods. can only point to methods with a given signature.
+                //delegates created in 3 ways: delegate keyword, anonymous method, lambda expression.
+                //we are using a lambda expression for this Action delegate.
+
+            //Map.Controllers scans codebase for any controllers & generates the appropriate routes to the endpoints in the controllers.
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
