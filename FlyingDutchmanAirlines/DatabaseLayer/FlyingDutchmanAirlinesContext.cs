09b4244a87fa7f4610123ea98252f84183ce7473
echao12 <echao0077@gmail.com>
2021-12-18 17:59:56 -0800

    retrieved connection string from environment variable
    -moved connection string to the user system environment.
    -used Environment.GetEnvVar(<key>) to fetch connection string from system.

--- FlyingDutchmanAirlines/DatabaseLayer/FlyingDutchmanAirlinesContext.cs
+++ FlyingDutchmanAirlines/DatabaseLayer/FlyingDutchmanAirlinesContext.cs
@@ -20,20 +20,28 @@ namespace FlyingDutchmanAirlines.DatabaseLayer
         {
         }
 
+        //DbSet types contain tables & their contents
         public virtual DbSet<Airport> Airports { get; set; }
         public virtual DbSet<Booking> Bookings { get; set; }
         public virtual DbSet<Customer> Customers { get; set; }
         public virtual DbSet<Flight> Flights { get; set; }
 
+        //on configuring is called on launch automatically on the dbcontext.
+        //it uses dependency injection to provide the DbContextOptionsBuiulder
+        // MAIN PURPOSE: configure settings related to database connection (ex: connection string).
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:codelikeacsharppro.database.windows.net,1433;Initial Catalog=FlyingDutchmanAirlines;Persist Security Info=False;User Id=dev;Password=FlyingDutchmanAirlines1972!;  MultipleActiveResultSets=False;Encrypt=True;  TrustServerCertificate=False;Connection Timeout=30;");
+                String connectionString = 
+                    //note: stored connection string as an enviroment variable to avoid hard coded connection strings.
+                    //note: "??" is null-coalescing operator. if not null, returns left value. else, right value.
+                    Environment.GetEnvironmentVariable("FlyingDutchmanAirlines_Database_Connection_String") ?? String.Empty; //grab connection string from system env. var.
+                optionsBuilder.UseSqlServer(connectionString);
             }
         }
 
+        //called during model creation
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
