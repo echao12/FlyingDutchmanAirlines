3bbc7b0d3322c4d1cba534ec171950ec7cdc1d6e
echao12 <echao0077@gmail.com>
2021-12-18 19:38:31 -0800

    Generated test solution to project
    used "dotnet new mstest" command to generate the testing solution.

---
+++ FlyingDutchmanAirlines_Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlyingDutchmanAirlines_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
