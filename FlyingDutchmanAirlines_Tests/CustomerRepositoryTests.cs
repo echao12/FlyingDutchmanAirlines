9822285cd1a2745da1d7611389818c7e8871a0d9
echao12 <echao0077@gmail.com>
2021-12-20 13:45:00 -0800

    created in-memory db for test project
    - added EFC InMemory package to test project.
    - implemented TestInitialize fn to generate in-mem db before each test is ran.
    - added in-mem db var called _context.

--- FlyingDutchmanAirlines_Tests/CustomerRepositoryTests.cs
+++ FlyingDutchmanAirlines_Tests/CustomerRepositoryTests.cs
@@ -1,12 +1,29 @@
 using System.Threading.Tasks;
 using FlyingDutchmanAirlines.RepositoryLayer; //to access methods for testing
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using FlyingDutchmanAirlines.DatabaseLayer;//for db context
+using Microsoft.EntityFrameworkCore.InMemory;
 
 namespace FlyingDutchmanAirlines_Tests.RepositoryLayer
 {
     [TestClass]
     public class CustomerRepositoryTest
     {
+        private FlyingDutchmanAirlinesContext _context;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            DbContextOptions<FlyingDutchmanAirlinesContext> dbContextOptions = 
+            // create new dbContextOptions using the builder pattern.
+               new DbContextOptionsBuilder<FlyingDutchmanAirlinesContext>()
+                // specify builder to use in-memory db called "FlyingDutchman"
+               .UseInMemoryDatabase("FlyingDutchman")
+               //fetch all of the final configured options
+               .Options;
+            _context = new FlyingDutchmanAirlinesContext(dbContextOptions);
+        }
+
         [TestMethod]
         public async Task CreateCustomer_Success() {
             //check for valid default object creation
