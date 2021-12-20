fa339df44bc582cae6b8565dcaaac70b3fdf59dd
echao12 <echao0077@gmail.com>
2021-12-19 17:20:40 -0800

    completed tests for null/empty name and name with invalid chars.
    - CreateCustomer() takes a string and checks for invalid names (null/empty/has invalid chars).
    - created 4 test methods.
    - valid input check.
    - null input check
    - empty string input check
    - contains invalid character input check

--- FlyingDutchmanAirlines/RepositoryLayer/CustomerRepository.cs
+++ FlyingDutchmanAirlines/RepositoryLayer/CustomerRepository.cs
@@ -1,7 +1,19 @@
+using System.Linq;
+
 namespace FlyingDutchmanAirlines.RepositoryLayer {
     public class CustomerRepository {
-        public void CreateCustomer(string name) {
-            
+        public bool CreateCustomer(string name) {
+            if(IsInvalidCustomerName(name)){
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsInvalidCustomerName(string str){
+            char[] forbiddenCharacters = {'!', '@', '#', '$', '%', '&', '*'};
+            //check for null/empty, then use linq to check each char for an invalid character.
+            return string.IsNullOrEmpty(str) || 
+                str.Any(x => forbiddenCharacters.Contains(x));
         }
     }
 }
--- FlyingDutchmanAirlines_Tests/CustomerRepositoryTests.cs
+++ FlyingDutchmanAirlines_Tests/CustomerRepositoryTests.cs
@@ -8,8 +8,45 @@ namespace FlyingDutchmanAirlines_Tests.RepositoryLayer
     {
         [TestMethod]
         public void CreateCustomer_Success() {
+            //check for valid default object creation
             CustomerRepository repository = new CustomerRepository();
             Assert.IsNotNull(repository);
+
+            bool result = repository.CreateCustomer("Rin");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CreateCustomer_Failure_NameIsNull() {
+            CustomerRepository repo = new CustomerRepository();
+            Assert.IsNotNull(repo);
+
+            bool result = repo.CreateCustomer(null);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CreateCustomer_Failure_NameIsEmpty() {
+            CustomerRepository repo = new CustomerRepository();
+            Assert.IsNotNull(repo);
+
+            bool result = repo.CreateCustomer("");
+            Assert.IsFalse(result);
+        }
+
+        //[DataRow] used to specify input parameter for test method.
+        [TestMethod]
+        [DataRow('#')]
+        [DataRow('$')]
+        [DataRow('%')]
+        [DataRow('&')]
+        [DataRow('*')]
+        public void CreateCustomer_Failure_NameContainsInvalidCharacters(char invalidCharacter){
+            CustomerRepository repo = new CustomerRepository();
+            Assert.IsNotNull(repo);
+
+            bool result = repo.CreateCustomer("Rin" + invalidCharacter);
+            Assert.IsFalse(result);
         }
     }
 }
